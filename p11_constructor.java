0a305125cbcfc9e1d8499d32d7a7d398b15f87b9
MEET-1008 <meetvaghasiya3229@gmail.com>
2023-02-16 08:11:41 +0530

    p11 constructor code add

---
+++ p11_constructor.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class person {
+
+  int agg;
+  int salary;
+  String name;
+
+  person() {
+    Scanner sc = new Scanner(System.in);
+    System.out.printf("Enter the your name :- ");
+    name = sc.nextLine();
+    System.out.printf("Enter the your agg :- ");
+    agg = sc.nextInt();
+    System.out.printf("Enter the your salary :-");
+    salary = sc.nextInt();
+    System.out.println(" \n" + "name is : " + name);
+    System.out.println(name + " agg is " + agg);
+    System.out.println(name + " salary is " + salary);
+  }
+
+  person(String name, int agg, int salary) {
+    this.name = name;
+    this.agg = agg;
+    this.salary = salary;
+    System.out.println("name is : " + name);
+    System.out.println(name + " agg is " + agg);
+    System.out.println(name + " salary is " + salary + "\n");
+  }
+}
+
+class p11_constructor {
+
+  public static void main(String args[]) {
+    person meet = new person("meet", 23, 23000);
+    person ob2 = new person();
+  }
+}
