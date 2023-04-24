091313fd4f577b7400860cd22b8bdb28dee9a8d0
MEET-1008 <meetvaghasiya3229@gmail.com>
2023-04-24 23:30:26 +0530

    code editing

--- p15_Dynamic_method.java
+++ p15_Dynamic_method.java
@@ -1,9 +1,10 @@
 import java.util.*;
  class A3 {
-    public void hello(String s) {                          // Dynamic_method 
+    public void hello(String s) {
         System.out.println("A3: Hello From " + s);
     }
 }
+
  class B3 extends A3 {
     @Override
     public void hello(String s) {
@@ -16,6 +17,7 @@ public class p15_Dynamic_method {
     public static void main(String[] args) {
         A3 a = new B3();
         a.hello("Dynamic Method Dispatch");
-      
+        B3 b = new B3();
+        b.hello("Method Overriding");
     }
 }
